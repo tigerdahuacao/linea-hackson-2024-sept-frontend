61013dd21857700db47dd62ad85db342fddc48bd
Wecle <837968732@qq.com>
2024-09-20 20:39:49 +0800

    refactor(images): use local path for avatar image source

--- src/page/Root/app.tsx
+++ src/page/Root/app.tsx
@@ -13,16 +13,11 @@ const App: FC = () => {
                 <div className="tw-flex tw-items-center tw-flex-col">
                     <div className="tw-flex-auto tw-my-5">
                         <div className="tw-text-neutral-800 ">
-                            <div className="tw-w-auto  tw-m-4  tw-bg-yellow-400 tw-rounded-full tw-flex tw-flex-row tw-p-3">
-                                <Avatar
-                                    alt="NARUTO"
-                                    src={`${
-                                        import.meta.env.PUBLIC_URL
-                                    }/images/anime/naruto/naruto-1.jpg`}
-                                />
+                            <div className="tw-w-auto  tw-m-4 tw-bg-yellow-400 tw-rounded-full tw-flex tw-flex-row tw-p-3">
+                                <Avatar alt="NARUTO" src="/images/anime/naruto/naruto-1.jpg" />
                                 <img
-                                    className="  h-8  object-contain inline-block"
-                                    src="@/assets/images/naruto/naruto-1.jpg"
+                                    className="tw-h-8 tw-object-contain tw-inline-block"
+                                    src="/images/anime/naruto/naruto-1.jpg"
                                     alt="img"
                                 />
                                 <button className="tw-w-full tw-text-gray-800 tw-py-2 tw-px-4 tw-font-bold hover:tw-bg-yellow-500 tw-rounded-full">
