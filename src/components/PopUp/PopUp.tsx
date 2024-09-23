49b96663dd30bd503b8bc33f062cf13439c970eb
Yuncong Qiang PayPal Laptop <yqiang@paypal.com>
2024-09-23 16:04:52 +0800

    fix[petro] fix close btn of PopUp

--- src/components/PopUp/PopUp.tsx
+++ src/components/PopUp/PopUp.tsx
@@ -1,7 +1,6 @@
 import IconButton from '@mui/material/IconButton';
 import { FC, ReactElement } from 'react';
 import CloseIcon from '@mui/icons-material/Close';
-import $style from './popup.module.scss';
 
 interface PopUpProps extends React.PropsWithChildren {
     openPopUp: boolean;
@@ -13,19 +12,21 @@ interface PopUpProps extends React.PropsWithChildren {
 //https://mui.com/material-ui/material-icons/
 const PopUp: FC<PopUpProps> = (props) => {
     return props.openPopUp ? (
-        <div className="tw-absolute tw-top-0 tw-left-0 tw-w-full tw-h-screen tw-flex tw-justify-center tw-items-center tw-bg-custom-popup-bg">
-            {/* Close Button */}
-            <div className="tw-right-4 tw-top-40 tw-absolute">
-                <IconButton
-                    onClick={() => {
-                        props.setOpenPopUp(false);
-                    }}
-                >
-                    <CloseIcon />
-                </IconButton>
-            </div>
-            <div className="tw-bg-slate-400 tw-w-full tw-p-32 tw-relative tw-max-w-7xl tw-opacity-70 tw-rounded-3xl">
-                {props.children}
+        <div className="tw-absolute tw-top-0 tw-left-0 tw-w-full tw-h-screen tw-flex tw-justify-center tw-items-center tw-bg-custom-popup-bg tw-flex-col tw-rounded-3xl">
+            <div className="tw-w-full tw-max-w-7xl tw-flex tw-justify-center tw-items-center tw-flex-col tw-bg-red-600 tw-rounded-3xl  tw-opacity-70">
+                {/* Close Button */}
+                <div className="tw-justify-end tw-flex tw-w-full tw-mt-4 tw-mr-4 tw-mb-2">
+                    <IconButton
+                        onClick={() => {
+                            props.setOpenPopUp(false);
+                        }}
+                    >
+                        <CloseIcon />
+                    </IconButton>
+                </div>
+                <div className=" tw-w-full tw-bg-zinc-600 tw-rounded-b-3xl tw-p-32 tw-relative tw-max-w-7xl tw-opacity-100 ">
+                    {props.children}
+                </div>
             </div>
         </div>
     ) : (
