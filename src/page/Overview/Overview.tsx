3c32c431df7d5145b1e2ed8c34baca89f7a44c40
Wecle <837968732@qq.com>
2024-09-30 23:20:45 +0900

    fix: modify file name

--- src/page/Overview/Overview.tsx
+++ src/page/Overview/Overview.tsx
@@ -5,7 +5,7 @@ import SnapWrapper from '@/components/ScrollSnap/SnapWrapper';
 import SnapAlign from '../../components/ScrollSnap/SnapAlign';
 
 import HomePage from './HomePage';
-import VoteCCList from './VoteList';
+import VoteList from './VoteList';
 
 const Overview: FC = () => {
     return (
@@ -14,7 +14,7 @@ const Overview: FC = () => {
                 <HomePage />
             </SnapAlign>
             <SnapAlign>
-                <VoteCCList />
+                <VoteList />
             </SnapAlign>
         </SnapWrapper>
     );
