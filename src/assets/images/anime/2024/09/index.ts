1f5cc69149ac8bbd2f08270bb794d66dca35270e
Wecle <837968732@qq.com>
2024-09-30 23:04:31 +0900

    refactor: optimize image introduction method

---
+++ src/assets/images/anime/2024/09/index.ts
@@ -0,0 +1,7 @@
+import chitandaImg from './chitanda.png';
+import mashiroImg from './mashiro.png';
+import misakaImg from './misaka.png';
+import rikkaImg from './rikka.png';
+import shinomiyaImg from './shinomiya.png';
+
+export { chitandaImg, mashiroImg, misakaImg, rikkaImg, shinomiyaImg };
---
+++ src/assets/images/anime/NFT-images/naruto-images/index.ts
@@ -0,0 +1,10 @@
+import naruto1 from './naruto-ai-gen-1.png';
+import naruto2 from './naruto-ai-gen-2.png';
+import naruto3 from './naruto-ai-gen-3.png';
+import naruto4 from './naruto-ai-gen-4.png';
+import naruto5 from './naruto-ai-gen-5.png';
+import naruto6 from './naruto-ai-gen-6.png';
+import naruto7 from './naruto-ai-gen-7.png';
+import naruto8 from './naruto-ai-gen-8.png';
+
+export { naruto1, naruto2, naruto3, naruto4, naruto5, naruto6, naruto7, naruto8 };
